555529ae33992094b67a9d919e8a976df5f4a703
MrNaveen669 <1320204008@bitraipur.ac.in>
2025-03-23 12:39:12 +0530

    frontend add

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import AllRoutes from "./Components/AllRoutes";
+// import logoIcon from './assets/logo.png';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <AllRoutes />
+    </div>
+  );
+}
+
+export default App;
