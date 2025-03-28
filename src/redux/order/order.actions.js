edb7adf4977c3b942daa8da9ca869d93df45f2ea
Mr Naveen <1320204008@bitraipur.ac.in>
2025-03-28 17:58:51 +0530

    admin panel added
    admin panel added

--- src/redux/order/order.actions.js
+++
@@ -1,8 +0,0 @@
-import { PLACED_ORDER } from "./order.types";
-
-export const addToOrder = (product) => {
-  return {
-    type: PLACED_ORDER,
-    payload: product,
-  };
-};
--- src/redux/order/order.types.js
+++
@@ -1,1 +0,0 @@
-export const PLACED_ORDER = "cart/items/add/";
